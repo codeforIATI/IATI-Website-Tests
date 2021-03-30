05ba654068d9f4fcf952f52b10a58563b9d023a2
Andy Lulham <a.lulham@gmail.com>
2021-03-30 18:25:40 +0100

    Remove datastore, query builder & validator tests

--- tests/test_query_builder.py
+++ tests/test_query_builder.py
@@ -1,55 +1,55 @@
-import pytest
-from utility import utility
-from web_test_base import WebTestBase
+# import pytest
+# from utility import utility
+# from web_test_base import WebTestBase
 
 
-class TestQueryBuilder(WebTestBase):
-    """
-    Test query builder (datastore.iatistandard.org/query/)
-    """
-    requests_to_load = {
-        'datastore.iatistandard.org/query/': {
-            'url': 'http://datastore.iatistandard.org/query/'
-        },
-        'POST Example': {
-            'url': 'http://datastore.iatistandard.org/query/index.php',
-            'method': 'POST',
-            'data': {
-                'format': 'activity',
-                'grouping': 'summary',
-                'sample-size': '50 rows',
-                'reporting-org[]': 'XM-DAC-3-1',
-                'sector[]': '12181',
-                'recipient-region[]': '298',
-                'submit': 'Submit'
-            }
-        },
-        'Publisher Information': {
-            'url': 'http://datastore.iatistandard.org/query/helpers/groups_cache_dc.json',
-            'min_response_size': 1500000
-        }
-    }
+# class TestQueryBuilder(WebTestBase):
+#     """
+#     Test query builder (datastore.iatistandard.org/query/)
+#     """
+#     requests_to_load = {
+#         'datastore.iatistandard.org/query/': {
+#             'url': 'http://datastore.iatistandard.org/query/'
+#         },
+#         'POST Example': {
+#             'url': 'http://datastore.iatistandard.org/query/index.php',
+#             'method': 'POST',
+#             'data': {
+#                 'format': 'activity',
+#                 'grouping': 'summary',
+#                 'sample-size': '50 rows',
+#                 'reporting-org[]': 'XM-DAC-3-1',
+#                 'sector[]': '12181',
+#                 'recipient-region[]': '298',
+#                 'submit': 'Submit'
+#             }
+#         },
+#         'Publisher Information': {
+#             'url': 'http://datastore.iatistandard.org/query/helpers/groups_cache_dc.json',
+#             'min_response_size': 1500000
+#         }
+#     }
 
-    @pytest.mark.parametrize("target_request", ["datastore.iatistandard.org/query/", "POST Example"])
-    def test_locate_links(self, target_request):
-        """
-        Confirm the page contains a link to:
+#     @pytest.mark.parametrize("target_request", ["datastore.iatistandard.org/query/", "POST Example"])
+#     def test_locate_links(self, target_request):
+#         """
+#         Confirm the page contains a link to:
 
-        * https://iatistandard.org/en/using-data/IATI-tools-and-resources/IATI-datastore/
-        """
-        req = self.loaded_request_from_test_name(target_request)
+#         * https://iatistandard.org/en/using-data/IATI-tools-and-resources/IATI-datastore/
+#         """
+#         req = self.loaded_request_from_test_name(target_request)
 
-        result = utility.get_links_from_page(req)
+#         result = utility.get_links_from_page(req)
 
-        assert "https://iatistandard.org/en/using-data/IATI-tools-and-resources/IATI-datastore/" in result
+#         assert "https://iatistandard.org/en/using-data/IATI-tools-and-resources/IATI-datastore/" in result
 
-    @pytest.mark.parametrize("target_request", ["POST Example"])
-    def test_form_submit_link(self, target_request):
-        """
-        Confirm result page contains a link to the relevant search.
-        """
-        req = self.loaded_request_from_test_name(target_request)
+#     @pytest.mark.parametrize("target_request", ["POST Example"])
+#     def test_form_submit_link(self, target_request):
+#         """
+#         Confirm result page contains a link to the relevant search.
+#         """
+#         req = self.loaded_request_from_test_name(target_request)
 
-        result = utility.get_links_from_page(req)
+#         result = utility.get_links_from_page(req)
 
-        assert "http://datastore.iatistandard.org/api/1/access/activity.csv?reporting-org=XM-DAC-3-1&sector=12181&recipient-region=298" in result
+#         assert "http://datastore.iatistandard.org/api/1/access/activity.csv?reporting-org=XM-DAC-3-1&sector=12181&recipient-region=298" in result
--- tests/test_validator.py
+++ tests/test_validator.py
@@ -1,85 +1,85 @@
-from lxml import etree
-import pytest
-from utility import utility
-from web_test_base import WebTestBase
+# from lxml import etree
+# import pytest
+# from utility import utility
+# from web_test_base import WebTestBase
 
 
-class TestIATIValidator(WebTestBase):
-    """
-    Test validator (validator.iatistandard.org)
-    """
-    requests_to_load = {
-        'validator.iatistandard.org': {
-            'url': 'http://validator.iatistandard.org/'
-        },
-        'Valid paste data': {
-            'url': 'http://validator.iatistandard.org/index.php',
-            'method': 'POST',
-            'data': {
-                'paste': utility.load_file_contents('data/valid.xml')
-            }
-        },
-        'Invalid paste data': {
-            'url': 'http://validator.iatistandard.org/index.php',
-            'method': 'POST',
-            'data': {
-                'paste': utility.load_file_contents('data/invalid.xml')
-            }
-        },
-        'Valid URL data': {
-            'url': 'http://validator.iatistandard.org/index.php',
-            'method': 'POST',
-            'data': {
-                'url': 'https://raw.githubusercontent.com/IATI/IATI-Extra-Documentation/version-2.01/en/activity-standard/activity-standard-example-annotated.xml'
-            }
-        },
-        'Invalid URL data': {
-            'url': 'http://validator.iatistandard.org/index.php',
-            'method': 'POST',
-            'data': {
-                'url': 'http://validator.iatistandard.org/index.php'
-            }
-        }
-    }
+# class TestIATIValidator(WebTestBase):
+#     """
+#     Test validator (validator.iatistandard.org)
+#     """
+#     requests_to_load = {
+#         'validator.iatistandard.org': {
+#             'url': 'http://validator.iatistandard.org/'
+#         },
+#         'Valid paste data': {
+#             'url': 'http://validator.iatistandard.org/index.php',
+#             'method': 'POST',
+#             'data': {
+#                 'paste': utility.load_file_contents('data/valid.xml')
+#             }
+#         },
+#         'Invalid paste data': {
+#             'url': 'http://validator.iatistandard.org/index.php',
+#             'method': 'POST',
+#             'data': {
+#                 'paste': utility.load_file_contents('data/invalid.xml')
+#             }
+#         },
+#         'Valid URL data': {
+#             'url': 'http://validator.iatistandard.org/index.php',
+#             'method': 'POST',
+#             'data': {
+#                 'url': 'https://raw.githubusercontent.com/IATI/IATI-Extra-Documentation/version-2.01/en/activity-standard/activity-standard-example-annotated.xml'
+#             }
+#         },
+#         'Invalid URL data': {
+#             'url': 'http://validator.iatistandard.org/index.php',
+#             'method': 'POST',
+#             'data': {
+#                 'url': 'http://validator.iatistandard.org/index.php'
+#             }
+#         }
+#     }
 
-    @pytest.mark.parametrize("target_request", ["validator.iatistandard.org"])
-    def test_contains_links(self, target_request):
-        """
-        Confirm the validator contains a link to iatistandard.org
-        """
-        response = self.loaded_request_from_test_name(target_request)
-        page_links = utility.get_links_from_page(response)
+#     @pytest.mark.parametrize("target_request", ["validator.iatistandard.org"])
+#     def test_contains_links(self, target_request):
+#         """
+#         Confirm the validator contains a link to iatistandard.org
+#         """
+#         response = self.loaded_request_from_test_name(target_request)
+#         page_links = utility.get_links_from_page(response)
 
-        assert "http://iatistandard.org/" in page_links
+#         assert "http://iatistandard.org/" in page_links
 
-    @pytest.mark.parametrize("target_request", ["validator.iatistandard.org"])
-    def test_contains_form(self, target_request):
-        """
-        Confirm the validator contains a form on each of three tabs.
-        """
-        response = self.loaded_request_from_test_name(target_request)
+#     @pytest.mark.parametrize("target_request", ["validator.iatistandard.org"])
+#     def test_contains_form(self, target_request):
+#         """
+#         Confirm the validator contains a form on each of three tabs.
+#         """
+#         response = self.loaded_request_from_test_name(target_request)
 
-        assert utility.locate_xpath_result(
-            response, '//*[@id="status"]/div/form')
-        assert utility.locate_xpath_result(
-            response, '//*[@id="fileTab"]/div/form')
-        assert utility.locate_xpath_result(
-            response, '//*[@id="extra"]/div/form')
+#         assert utility.locate_xpath_result(
+#             response, '//*[@id="status"]/div/form')
+#         assert utility.locate_xpath_result(
+#             response, '//*[@id="fileTab"]/div/form')
+#         assert utility.locate_xpath_result(
+#             response, '//*[@id="extra"]/div/form')
 
-    @pytest.mark.parametrize("target_request", ["Valid paste data", "Valid URL data"])
-    def test_valid_input_xml(self, target_request):
-        """
-        Confirm the validator correctly identifies valid XML as such.
-        """
-        response = self.loaded_request_from_test_name(target_request)
-        text = utility.get_text_from_xpath(response, '//*[@id="status"]/div[2]')
-        assert text[0].strip() == "This is a well formed xml file."
+#     @pytest.mark.parametrize("target_request", ["Valid paste data", "Valid URL data"])
+#     def test_valid_input_xml(self, target_request):
+#         """
+#         Confirm the validator correctly identifies valid XML as such.
+#         """
+#         response = self.loaded_request_from_test_name(target_request)
+#         text = utility.get_text_from_xpath(response, '//*[@id="status"]/div[2]')
+#         assert text[0].strip() == "This is a well formed xml file."
 
-    @pytest.mark.parametrize("target_request", ["Invalid paste data", "Invalid URL data"])
-    def test_invalid_input_xml(self, target_request):
-        """
-        Confirm the validator correctly identifies invalid XML as such.
-        """
-        response = self.loaded_request_from_test_name(target_request)
-        text = utility.get_text_from_xpath(response, '//*[@id="status"]/div[2]')
-        assert text[0].strip() == "This is not a well-formed xml file"
+#     @pytest.mark.parametrize("target_request", ["Invalid paste data", "Invalid URL data"])
+#     def test_invalid_input_xml(self, target_request):
+#         """
+#         Confirm the validator correctly identifies invalid XML as such.
+#         """
+#         response = self.loaded_request_from_test_name(target_request)
+#         text = utility.get_text_from_xpath(response, '//*[@id="status"]/div[2]')
+#         assert text[0].strip() == "This is not a well-formed xml file"
